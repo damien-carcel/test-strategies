ebe1f637c802664113c946bab6e584ee2467ca7b
Damien Carcel <damien.carcel@obat.fr>
2024-11-18 16:04:17 +0100

    tech: introduce the "in memory" Symfony environment
    - Run integration tests under both environments.
    - Test it behaves correctly with a minimal test for the User
      repositories.

---
+++ demo/tests/Integration/AbstractIntegrationTestCase.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Integration;
+
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Symfony\Component\HttpKernel\KernelInterface;
+
+abstract class AbstractIntegrationTestCase extends KernelTestCase
+{
+    /**
+     * @param array<string, string> $options
+     */
+    protected static function bootKernel(array $options = []): KernelInterface
+    {
+        /** @phpstan-var string $testEnvironment */
+        $testEnvironment = $_ENV['TEST_ENV'] ?? throw new \RuntimeException(
+            'TEST_ENV environment variable not found.',
+        );
+
+        if (!\in_array($testEnvironment, ['memory', 'test'], true)) {
+            throw new \RuntimeException(
+                "Invalid TEST_ENV environment variable value \"$testEnvironment\". Valid values are \"in-memory\" and \"test\".",
+            );
+        }
+
+        $options = array_merge($options, ['environment' => $testEnvironment]);
+
+        return parent::bootKernel($options);
+    }
+}
